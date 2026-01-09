7ab1a0b6754d77e262e6c54bf07c8919d03a808e
Shashank Vinayak <mrobsessed01@Shashanks-MacBook-Air.local>
2026-01-09 13:14:49 +0200

    First commit in Desktop ig

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'; // Add useState import here
 import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  const [leakageData, setLeakageData] = useState(null);  // useState now works
+  useEffect(() => {
+    fetch(`${apiUrl}/water-leakage`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);  // Check the data received
+        setLeakageData(data);  // Update the state with the received data
+      })
+      .catch(error => console.error("Error fetching data:", error));
+  }, [apiUrl]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Water Leakage Detection System</h1>
+      <h2> Team Prinsys</h2>
+      <div>
+        <h3>{leakageData ? (leakageData.leakageDetected ? 'Leakage Detected!' : 'No Leakage detected') : 'Loading data...'}</h3>
+        {leakageData && (
+          <>
+            <p>Sensor ID: {leakageData.sensor_id}</p>
+            <p>Timestamp: {new Date(leakageData.timestamp).toLocaleString()}</p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
