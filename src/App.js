960e53d7f3b8054ab494631ca0d80e25cb62ca56
Niek Adams <9772100+NiekAdams@users.noreply.github.com>
2026-01-10 14:16:21 +0000

    Design front-end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { createClient } from '@supabase/supabase-js'
+import { AnimatePresence, motion } from "framer-motion";
 import './App.css';
 
-document.body.style.backgroundColor = "#222831";
+document.body.style.background = "linear-gradient(rgba(34, 40, 49, 0.8), rgba(34, 40, 49, 0.8)), url('./bg.jpg')";
+document.body.style.backgroundSize = "cover"
 const supabase = createClient('https://usuujbwztwljaytklyws.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdXVqYnd6dHdsamF5dGtseXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwMjgyOTYsImV4cCI6MjA4MzYwNDI5Nn0.nD90ciMvi2_8f5KtAnenjn_jStRwu2FnFkXtWezQ2iA')
 
 // Create a single supabase client for interacting with your database
@@ -40,15 +42,29 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <div className={"pr-6 pl-3 fixed left-2 bottom-2 flex justify-between w-full"}>
+        <img className={"h-16"} src="prinsys.png"></img>
+        <img className={"h-16"} src="aalto.png"></img>
+      </div>
       <h1 class="text-5xl">Team Prinsys</h1>
       <h2 class="text-2xl">Water Leakage Detection System</h2>
       <div>
         <button onClick={() => setAlerts([])} className="hover:to-red-500 active:to-red-700 active:from-red-700 px-8 py-3 mt-8  rounded text-white bg-gradient-to-b from-red-500 to-red-600">Clear all alerts</button>
-        <button onClick={() => setAlerts([])} className="hover:to-green-500 active:to-green-700 active:from-green-700 px-8 py-3 mt-8  rounded text-white bg-gradient-to-b from-green-500 to-green-600">Resolve all alerts</button>
-        <div className="flex flex-col-reverse gap-4 w-[50%] m-auto my-8">
+        <div className="flex flex-col-reverse gap-4 md:w-[50%] m-auto my-8">
+          <AnimatePresence initial={false}>
           {alerts.map((msg, index) => (
+            <motion.div
+              key={alert.time}
+              layout
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
             <Alert room={msg.room} location={msg.location} datetime={msg.time} />
+            </motion.div>
           ))}
+          </AnimatePresence>
         </div>
       </div>
     </div>
@@ -66,7 +82,7 @@ function Alert({ building = "Building 3", room, location, datetime, isResolved =
   }, []);
 
   return (
-    <div className={"flex flex-col items-start p-3 rounded bg-gradient-to-b from-[#393E46] to-[#2e333b] alert-box cursor-pointer " + (flash ? "animate-bounce" : "")
+    <div className={"flex flex-col items-start p-3 rounded bg-gradient-to-b from-[#393E46] to-[#2e333b] alert-box cursor-pointer"
   }
          onClick={() => setResolved(prev => !prev)}
       >
